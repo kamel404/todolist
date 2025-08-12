47514e5604ce1fc29b03ed8efc24a404ee24ad29
kamel404 <10121641@mu.edu.lb>
2025-08-12 15:19:18 +0300

    tailwind configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div class="flex flex-col items-center p-7 rounded-2xl">
+  <div>
+    <img class="size-48 shadow-xl rounded-md" alt="" src="/img/cover.png" />
+  </div>
+  <div class="flex">
+    <span class="text-2xl font-medium">Class Warfare</span>
+    <span class="font-medium text-sky-500">The Anti-Patterns</span>
+    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div>
   )
 }
 
